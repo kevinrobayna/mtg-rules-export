f1a5c9306c4bbea8aca269059e294d8890aa442f
Kevin Robayna <me@kevinrobayna.com>
2022-07-06 13:49:42 +0100

    fix specs

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-    clearMocks: true,
-    moduleFileExtensions: ['js', 'ts'],
-    testMatch: ['**/*.test.ts'],
-    transform: {
-      '^.+\\.ts$': 'ts-jest'
-    },
-    verbose: true
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.test.{ts,tsx}', '!**/dist/**'],
+  coverageThreshold: {
+    global: {
+      lines: 70
+    }
   }
+}
