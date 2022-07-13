60ccd217c59df510df49c639cd47752191498c5f
Kevin Robayna <me@kevinrobayna.com>
2022-07-13 22:39:40 +0100

    fix rules code

--- src/scrape/rules.ts
+++ src/scrape/rules.ts
@@ -1,33 +1,38 @@
 import * as core from '@actions/core'
 import * as cheerio from 'cheerio'
+import axios from 'axios'
 
 export const RULES_URI = 'https://magic.wizards.com/en/rules/'
 
 async function getRulesWebsite(): Promise<string> {
-  core.info(`Fetching rules url from ${RULES_URI}...`)
-  const response = await fetch(RULES_URI)
-  core.info(`Requested rules url ${RULES_URI}... status=${response.status}`)
-  if (response.ok) {
-    return await response.text()
-  } else {
-    throw new Error('Could not load MTG Rules website')
+  try {
+    core.info(`Fetching rules url from ${RULES_URI}...`)
+    const {data, status} = await axios.get(RULES_URI)
+    core.info(`Requested rules url ${RULES_URI}... status=${status}`)
+    return data
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      core.info(`error message: ${error.message}`)
+      throw error
+    } else {
+      core.info(`unexpected error: ${error}`)
+      throw new Error(`Could not load MTG Rules website`)
+    }
   }
 }
 
 async function scrapeRulesWebsite(): Promise<string> {
-  const html = await getRulesWebsite()
+  const html: string = await getRulesWebsite()
   const $ = cheerio.load(html)
-  return $('.cta')
-    .toArray()
-    .map(anchor => {
-      return anchor.attribs['href']
+  const urls: string[] = $('.cta', html)
+    .map((_i, element: cheerio.Element) => {
+      if (element.type === 'tag') {
+        return element.attribs.href
+      }
     })
-    .map(link => {
-      return link.replaceAll(' ', '%20')
-    })
-    .find(link => {
-      return link.endsWith('.txt')
-    })
+    .toArray() as unknown as string[]
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return urls.map(e => e.replace(/\s/g, '%20')).find(e => e.endsWith('.txt'))!
 }
 
 export async function getRulesUrl(): Promise<string> {
@@ -36,5 +41,5 @@ export async function getRulesUrl(): Promise<string> {
     throw new Error('There was no link to the rules')
   }
   core.setOutput('rules_uri', rules_uri)
-  return rules_uri
+  return rules_uri.toString()
 }
--- tests/scrape/rules.test.ts
+++ tests/scrape/rules.test.ts
@@ -1,70 +1,64 @@
 import * as fs from 'fs'
-import {expect, describe, it} from '@jest/globals'
+import axios, {AxiosError} from 'axios'
+import {jest, expect, describe, it} from '@jest/globals'
 
-import {getRulesUrl, RULES_URI} from '../src/scrape/rules'
+import {getRulesUrl, RULES_URI} from '../../src/scrape/rules'
+
+jest.mock('axios')
+const mockAxios = axios as jest.Mocked<typeof axios>
 
 describe('getRulesUrl', () => {
-  global.fetch = jest.fn(() => {})
-
-  afterEach(() => {
-    fetch.mockClear()
-  })
-
   it('should return url given a copy of the real mtg website', async () => {
     // given a copy of the real mtg website
-    fetch.mockImplementationOnce(() =>
-      Promise.resolve({
-        status: 200,
-        ok: true,
-        text: () => Promise.resolve(fs.readFileSync('src/__tests__/rules.html', 'utf8'))
-      })
-    )
+    mockAxios.get.mockResolvedValue({
+      status: 200,
+      data: fs.readFileSync('./tests/rules.html', 'utf8')
+    })
 
     // when we try to get the rules url
     const rules_uri = await getRulesUrl()
 
     // then, we called the right url
-    expect(fetch).toHaveBeenCalledWith(RULES_URI)
+    expect(mockAxios.get).toHaveBeenCalledWith(RULES_URI)
 
     // and we got the url for the rules text file
     expect(rules_uri).toBe('https://media.wizards.com/2022/downloads/MagicCompRules%2020220610.txt')
   })
 
-  it('returns an error when we fail to load mtg website', async () => {
+  it('returns an error when we get an axios error', async () => {
     // given that Wizards is having a bad day
-    fetch.mockImplementationOnce(() =>
-      Promise.resolve({
-        status: 500,
-        ok: false
-      })
-    )
+    mockAxios.get.mockRejectedValue(new AxiosError('Error'))
 
     // when we try to get the rules url
-    try {
-      await getRulesUrl()
-    } catch (error) {
-      expect(error.message).toBe('Could not load MTG Rules website')
-    }
+    await expect(getRulesUrl()).rejects.toThrow('Could not load MTG Rules website')
 
     // and we got the url for the rules text file
-    expect(fetch).toHaveBeenCalledWith(RULES_URI)
+    expect(mockAxios.get).toHaveBeenCalledWith(RULES_URI)
+  })
+
+  it('returns an error when we fail to load mtg website', async () => {
+    // given that Wizards is having a bad day
+    mockAxios.get.mockRejectedValue(new Error('Error'))
+
+    // when we try to get the rules url
+    await expect(getRulesUrl()).rejects.toThrow('Could not load MTG Rules website')
+
+    // and we got the url for the rules text file
+    expect(mockAxios.get).toHaveBeenCalledWith(RULES_URI)
   })
 
   it('returns a %20 for each space', async () => {
     // given that the text url contains several spaces
-    fetch.mockImplementationOnce(() =>
-      Promise.resolve({
-        status: 200,
-        ok: true,
-        text: () => Promise.resolve(`<a class="cta" href="https://rules   abc.txt"></a>`)
-      })
-    )
+    mockAxios.get.mockResolvedValue({
+      status: 200,
+      data: `<a class="cta" href="https://rules   abc.txt"></a>`
+    })
 
     // when we try to get the rules url
     const rules_uri = await getRulesUrl()
 
     // then, we called the right url
-    expect(fetch).toHaveBeenCalledWith(RULES_URI)
+    expect(mockAxios.get).toHaveBeenCalledWith(RULES_URI)
 
     // and we got the url for the rules text file
     expect(rules_uri).toBe('https://rules%20%20%20abc.txt')
@@ -72,22 +66,35 @@ describe('getRulesUrl', () => {
 
   it("returns an error when there's no link to the rules", async () => {
     // given that the text url contains several spaces
-    fetch.mockImplementationOnce(() =>
-      Promise.resolve({
-        status: 200,
-        ok: true,
-        text: () => Promise.resolve(`<a class="another_link" href="http://ecosia.org"></a>`)
-      })
-    )
+    mockAxios.get.mockResolvedValue({
+      status: 200,
+      data: `<a class="another_link" href="http://ecosia.org"></a>`
+    })
 
     // when we try to get the rules url
-    try {
-      await getRulesUrl()
-    } catch (error) {
-      expect(error.message).toBe('There was no link to the rules')
-    }
+    await expect(getRulesUrl()).rejects.toThrow('There was no link to the rules')
 
     // and we got the url for the rules text file
-    expect(fetch).toHaveBeenCalledWith(RULES_URI)
+    expect(mockAxios.get).toHaveBeenCalledWith(RULES_URI)
+  })
+
+  it('returns the first website when there are more than one txt urls', async () => {
+    // given that the text url contains several spaces
+    mockAxios.get.mockResolvedValue({
+      status: 200,
+      data: `
+      <a class="cta" href="http://1.txt"></a>
+      <a class="cta" href="http://2.txt"></a>
+      `
+    })
+
+    // when we try to get the rules url
+    const rules_uri = await getRulesUrl()
+
+    // then, we called the right url
+    expect(mockAxios.get).toHaveBeenCalledWith(RULES_URI)
+
+    // and we get the first url ending in txt
+    expect(rules_uri).toBe('http://1.txt')
   })
 })
