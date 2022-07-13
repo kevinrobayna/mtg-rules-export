e6f4531f63e477bf188fa56329d7071cdd2dad19
Kevin Robayna <me@kevinrobayna.com>
2022-07-13 22:43:33 +0100

    Revert "remove test trying to run gh action"
    This reverts commit 5551db32e48b903b47344f094ec4859923ff7c2e.

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -7,6 +7,16 @@ import {run} from '../src/main'
 
 // shows how the runner will run a javascript action with env / stdout protocol
 describe('Github Action', () => {
+  it('should run without any errors as process', () => {
+    process.env['INPUT_MILLISECONDS'] = '500'
+    const np = process.execPath
+    const ip = path.join('lib', 'main.js')
+    const options: cp.ExecFileSyncOptions = {
+      env: process.env
+    }
+    console.log(cp.execFileSync(np, [ip], options).toString())
+  })
+
   it('should run without any errors', async () => {
     await run()
   })
