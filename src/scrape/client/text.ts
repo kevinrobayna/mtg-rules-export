58927e86c1db56b58e98528e952e222ca932bb37
Kevin Robayna <me@kevinrobayna.com>
2022-07-14 22:12:00 +0100

    add specs on rules txt client

--- src/scrape/client/text.ts
+++ src/scrape/client/text.ts
@@ -6,6 +6,9 @@ export default async function getRulesText(url: string): Promise<string> {
     core.info(`Fetching rules url from ${url}...`)
     const {data, status} = await axios.get(url)
     core.info(`Requested rules url ${url}... status=${status}`)
+    if (status !== 200) {
+      throw new Error(`Could not load rules text from ${url}`)
+    }
     return data
   } catch (error) {
     core.error(`unexpected error: ${error}`)
