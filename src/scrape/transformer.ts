e4cd4b133d97121d349c6c7c7e34592bf6d57b76
Kevin Robayna <me@kevinrobayna.com>
2022-07-17 22:03:47 +0100

    add method to fetch effective date of rules

--- src/scrape/transformer.ts
+++ src/scrape/transformer.ts
@@ -1,3 +1,32 @@
-export default function rulesToJson(rules_txt: string): string {
-  return rules_txt
+interface Rules {
+  effective_date: string
+}
+
+const EFFECTIVE_DATE_TXT = 'These rules are effective as of'
+
+export default function rulesToJson(rules_txt: string): Rules {
+  const rules_lines = formatRules(rules_txt)
+  const output: Rules = {
+    effective_date: getEffectiveDate(rules_lines)
+  }
+  return output
+}
+
+function getEffectiveDate(rules_txt: string[]): string {
+  const line: string = rules_txt.find(it => it.includes(EFFECTIVE_DATE_TXT)) || ''
+  return line.replace(EFFECTIVE_DATE_TXT, '').replace('.', '').trim()
+}
+
+function formatRules(txt: string): string[] {
+  const cleanedUpRules = txt
+    .replace(/“/g, '"')
+    .replace(/”/g, '"')
+    .replace(/’/g, "'")
+    .replace(/‘/g, "'")
+    .replace(/—/g, '-')
+    // Replaces empty lines before effective date comment
+    // check https://regex101.com for more info
+    .replace(/\n\s{4,}(\w)/g, ' $1')
+
+  return cleanedUpRules.split('\n')
 }
--- tests/scrape/transformer.test.ts
+++ tests/scrape/transformer.test.ts
@@ -2,14 +2,10 @@ import {describe, expect, it} from '@jest/globals'
 import * as fs from 'fs'
 import rulesToJson from '../../src/scrape/transformer'
 
-const COMPLETE_RULES : string = fs.readFileSync('./tests/scrape/rules.txt', 'utf8')
+const COMPLETE_RULES: string = fs.readFileSync('./tests/scrape/rules.txt', 'utf8')
 
 describe('rulesToJson', () => {
-  it('should return json with rules in a computer readable format',  () => {
-    // when we try to get the rules url
-    const rules_txt = rulesToJson(COMPLETE_RULES)
-
-    // and we got the url for the rules text file
-    expect(rules_txt).toContain('Magic: The Gathering Comprehensive Rules')
+  it('should return effective date of rules', () => {
+    expect(rulesToJson(COMPLETE_RULES).effective_date).toBe('July 8, 2022')
   })
 })
